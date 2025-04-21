a4e95b703834bdc9ee63f84010dd0643361bfbe2
NikBig0 <135820140+NikBig0@users.noreply.github.com>
2025-04-22 00:41:13 +0300

    Add files via upload

---
+++ Moduls/AdminClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Lombard.Moduls
+{
+    public class AdminClass
+    {
+
+        public static Frame MainFrame { get; set; }
+
+        public static User CurrentUser { get; set; }
+
+    }
+}
